4a3285d5f9637d852935253da2e371d878215c64
stutxo <70952638+stutxo@users.noreply.github.com>
2024-03-26 02:30:21 +0000

    add cargo release params

--- dev.sh
+++ dev.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cargo build --target wasm32-unknown-unknown --release
-wasm-tools strip target/wasm32-unknown-unknown/release/blockwars.wasm -t
+wasm-strip target/wasm32-unknown-unknown/release/blockwars.wasm
 wasm-opt -o docs/blockwars.wasm -Oz target/wasm32-unknown-unknown/release/blockwars.wasm
 ls -lh docs
 python3 -m http.server --directory docs 1334
