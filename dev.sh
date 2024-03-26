681e36cc484cab295bdc3b2b2c58be85742f3533
stutxo <70952638+stutxo@users.noreply.github.com>
2024-03-26 03:43:47 +0000

    remove dev file, we dont need it

--- dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cargo build --target wasm32-unknown-unknown --release
-wasm-strip target/wasm32-unknown-unknown/release/blockwars.wasm
-wasm-opt -o docs/blockwars.wasm -Oz target/wasm32-unknown-unknown/release/blockwars.wasm
-ls -lh docs
-python3 -m http.server --directory docs 1334
-
-
