89e539025927252893582a5af23267ae8aa2742a
stutxo <70952638+stutxo@users.noreply.github.com>
2024-03-26 02:46:26 +0000

    moreeee optimize

--- dev.sh
+++ dev.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cargo build --target wasm32-unknown-unknown --release
-wasm-strip target/wasm32-unknown-unknown/release/blockwars.wasm
-wasm-opt -o docs/blockwars.wasm -Oz target/wasm32-unknown-unknown/release/blockwars.wasm
+wasm-strip target/wasm32-unknown-unknown/release/b.wasm
+wasm-opt -o docs/blockwars.wasm -Oz target/wasm32-unknown-unknown/release/b.wasm
 ls -lh docs
 python3 -m http.server --directory docs 1334
 
--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,6 @@ fn handle_panic(_: &core::panic::PanicInfo) -> ! {
     loop {}
 }
 #[no_mangle]
-pub extern "C" fn the_answer() -> u32 {
+pub extern "C" fn a() -> u32 {
     42
 }
