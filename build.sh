7567336d27f939163c460fff9db1a17c79a1222f
stutxo <70952638+stutxo@users.noreply.github.com>
2024-03-26 02:50:17 +0000

    moreeee optimize

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cargo build --target wasm32-unknown-unknown --release
-wasm-strip target/wasm32-unknown-unknown/release/b.wasm
-wasm-opt -o docs/blockwars.wasm -Oz target/wasm32-unknown-unknown/release/b.wasm
+wasm-strip target/wasm32-unknown-unknown/release/blockwars.wasm
+wasm-opt -o docs/blockwars.wasm -Oz target/wasm32-unknown-unknown/release/blockwars.wasm
 ls -lh docs/
 
 # testing something
