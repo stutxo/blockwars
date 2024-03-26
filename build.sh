c8203dee2ebbd47e45419fee1cee500229f2e870
stutxo <70952638+stutxo@users.noreply.github.com>
2024-03-26 03:46:23 +0000

    small change to build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 cargo build --target wasm32-unknown-unknown --release
 wasm-strip target/wasm32-unknown-unknown/release/blockwars.wasm
 wasm-opt -o docs/blockwars.wasm -Oz target/wasm32-unknown-unknown/release/blockwars.wasm
-ls -lh docs/
+ls -lh docs/blockwars.wasm | awk '{print "wasm size: " $5}'
 
 # testing something
 # base64 -i docs/blockwars.wasm -o blockwars.wasm.base64
