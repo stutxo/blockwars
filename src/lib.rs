ca7ee0976c5b87f30f78b39075adbc4a0f98386f
stutxo <70952638+stutxo@users.noreply.github.com>
2024-03-26 03:03:00 +0000

    test

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,31 @@
 #![no_std]
 
+use core::sync::atomic::{AtomicU32, Ordering};
+
+static FRAME: AtomicU32 = AtomicU32::new(0);
+
+const WIDTH: usize = 600;
+const HEIGHT: usize = 600;
+
+#[no_mangle]
+static mut BUFFER: [u32; WIDTH * HEIGHT] = [0; WIDTH * HEIGHT];
+
+#[no_mangle]
+pub unsafe extern "C" fn go() {
+    render_frame_safe(&mut BUFFER)
+}
+
+fn render_frame_safe(buffer: &mut [u32; WIDTH * HEIGHT]) {
+    let f = FRAME.fetch_add(1, Ordering::Relaxed);
+
+    for y in 0..HEIGHT {
+        for x in 0..WIDTH {
+            buffer[y * WIDTH + x] = f.wrapping_add((x ^ y) as u32) | 0xFF_00_00_00;
+        }
+    }
+}
+
 #[panic_handler]
 fn handle_panic(_: &core::panic::PanicInfo) -> ! {
     loop {}
-}
-#[no_mangle]
-pub extern "C" fn a() -> u32 {
-    42
 }
