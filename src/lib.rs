a9821a0b5c46a56ac2c01cd86cf2f393aad11ae8
stutxo <70952638+stutxo@users.noreply.github.com>
2024-03-26 17:23:47 +0000

    input working for desktop

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,12 @@
 
 use core::sync::atomic::{AtomicU32, Ordering};
 
-static FRAME: AtomicU32 = AtomicU32::new(0);
+static SQUARE_POS_X: AtomicU32 = AtomicU32::new(WIDTH as u32 / 2 - 5);
+static SQUARE_POS_Y: AtomicU32 = AtomicU32::new(HEIGHT as u32 / 2 - 5);
 
-const WIDTH: usize = 600;
-const HEIGHT: usize = 600;
+const WIDTH: usize = 800;
+const HEIGHT: usize = 800;
+const SPEED: u32 = 10;
 
 #[no_mangle]
 static mut BUFFER: [u32; WIDTH * HEIGHT] = [0; WIDTH * HEIGHT];
@@ -15,12 +17,58 @@ pub unsafe extern "C" fn go() {
     render_frame_safe(&mut BUFFER)
 }
 
-fn render_frame_safe(buffer: &mut [u32; WIDTH * HEIGHT]) {
-    let f = FRAME.fetch_add(1, Ordering::Relaxed);
+pub enum Key {
+    Left,
+    Right,
+    Up,
+    Down,
+}
 
-    for y in 0..HEIGHT {
-        for x in 0..WIDTH {
-            buffer[y * WIDTH + x] = f.wrapping_add((x ^ y) as u32) | 0xFF_00_00_00;
+#[no_mangle]
+pub unsafe extern "C" fn key_pressed(value: usize) {
+    let key = match value {
+        1 => Key::Left,
+        2 => Key::Right,
+        3 => Key::Up,
+        4 => Key::Down,
+        _ => return,
+    };
+
+    match key {
+        Key::Left => {
+            SQUARE_POS_X
+                .fetch_update(Ordering::SeqCst, Ordering::SeqCst, |x| Some(x - 1 * SPEED))
+                .unwrap();
+        }
+        Key::Right => {
+            SQUARE_POS_X
+                .fetch_update(Ordering::SeqCst, Ordering::SeqCst, |x| Some(x + 1 * SPEED))
+                .unwrap();
+        }
+        Key::Up => {
+            SQUARE_POS_Y
+                .fetch_update(Ordering::SeqCst, Ordering::SeqCst, |y| Some(y - 1 * SPEED))
+                .unwrap();
+        }
+        Key::Down => {
+            SQUARE_POS_Y
+                .fetch_update(Ordering::SeqCst, Ordering::SeqCst, |y| Some(y + 1 * SPEED))
+                .unwrap();
+        }
+    }
+}
+
+fn render_frame_safe(buffer: &mut [u32; WIDTH * HEIGHT]) {
+    for i in 0..(WIDTH * HEIGHT) {
+        buffer[i] = 0xFF_00_00_00;
+    }
+
+    let start_x = SQUARE_POS_X.load(Ordering::Relaxed) as usize;
+    let start_y = SQUARE_POS_Y.load(Ordering::Relaxed) as usize;
+
+    for y in start_y..(start_y + 10) {
+        for x in start_x..(start_x + 10) {
+            buffer[y * WIDTH + x] = 0xFFFFFF;
         }
     }
 }
