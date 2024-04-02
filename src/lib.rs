cec972371b631c31b780b462089772d237ce7bb1
stutxo <70952638+stutxo@users.noreply.github.com>
2024-04-02 07:11:35 +0100

    new gameplay stuff

--- src/lib.rs
+++ src/lib.rs
@@ -3,27 +3,24 @@ use core::ptr;
 
 const WIDTH: u8 = 255;
 const HEIGHT: u8 = 255;
-const SEED: u32 = 0x1337;
 
-static mut PLAYER: (u8, u8, u8) = (WIDTH / 2 - 5, (HEIGHT / 4) * 3, 1);
+static mut TELEPORT: [(u8, u8, u8); MAX_TELEPORT] = [(0, 0, 0); MAX_TELEPORT];
+const MAX_TELEPORT: usize = 8;
+const TELEPORT_SIZE: u8 = 15;
+const TELEPORT_SPEED: u8 = 5;
 
-const PLAYER_SIZE: u8 = 5;
-const PLAYER_SPEED: u32 = 3;
+static mut GRID: [bool; GRID_WIDTH * GRID_HEIGHT] = [false; GRID_WIDTH * GRID_HEIGHT];
+const GRID_WIDTH: usize = (WIDTH as usize) / TELEPORT_SIZE as usize;
+const GRID_HEIGHT: usize = (HEIGHT as usize) / TELEPORT_SIZE as usize;
 
-static mut ENEMIES: [(u8, u8, u8); MAX_ENEMIES] = [(0, 0, 0); MAX_ENEMIES];
-const ENEMY_SIZE: u8 = 4;
-const ENEMIES_PER_WAVE: u8 = 1;
-const MAX_ENEMIES: usize = 255 * 255 / 25;
-const ENEMY_SPAWN_RATE: u32 = 10;
-const ENEMY_SPEED: u32 = 2;
-
-static mut KEY_STATE: u8 = 0;
+static mut KEY_STATE: bool = false;
 static mut FRAME: u32 = 0;
+static mut SEED: u32 = 0;
 
 //https://blog.orhun.dev/zero-deps-random-in-rust/
 #[inline]
-fn rng(frame: u32) -> impl Iterator<Item = u32> {
-    let mut random = SEED + frame;
+fn rng(seed: u32, frame: u32) -> impl Iterator<Item = u32> {
+    let mut random = seed + frame;
     core::iter::repeat_with(move || {
         random ^= random << 13;
         random ^= random >> 17;
@@ -38,8 +35,14 @@ static mut BUFFER: [u32; 255 * 255] = [0; 255 * 255];
 
 #[inline]
 #[no_mangle]
-unsafe extern "C" fn key_pressed(value: u8) {
-    KEY_STATE = value;
+unsafe extern "C" fn key_pressed() {
+    KEY_STATE = true;
+}
+
+#[inline]
+#[no_mangle]
+unsafe extern "C" fn seed(value: u32) {
+    SEED = value;
 }
 
 #[inline]
@@ -48,11 +51,13 @@ unsafe extern "C" fn game_loop() -> u32 {
     FRAME += 1;
     frame_safe(
         &mut *ptr::addr_of_mut!(BUFFER),
-        &mut *ptr::addr_of_mut!(ENEMIES),
-        &mut *ptr::addr_of_mut!(PLAYER),
         FRAME,
-        KEY_STATE,
+        &mut *ptr::addr_of_mut!(KEY_STATE),
+        SEED,
+        &mut *ptr::addr_of_mut!(TELEPORT),
+        &mut *ptr::addr_of_mut!(GRID),
     );
+
     1
 }
 
@@ -60,107 +65,192 @@ unsafe extern "C" fn game_loop() -> u32 {
 #[inline]
 fn frame_safe(
     buffer: &mut [u32; 255 * 255],
-    enemies: &mut [(u8, u8, u8); MAX_ENEMIES],
-    player: &mut (u8, u8, u8),
     frame: u32,
-    key_state: u8,
+    key_state: &mut bool,
+    seed: u32,
+    teleport: &mut [(u8, u8, u8); MAX_TELEPORT],
+    grid: &mut [bool; GRID_WIDTH * GRID_HEIGHT],
 ) {
-    let mut rng = rng(frame);
+    let mut rng = rng(seed, frame);
 
-    if frame % ENEMY_SPAWN_RATE == 0 {
-        spawn_enemy(enemies, &mut rng);
+    if frame == 1 {
+        spawn_teleporters_on_grid(teleport, &mut rng, grid);
     }
-    if frame % PLAYER_SPEED == 0 {
-        update_player_pos(player, key_state);
-    }
-    if frame % ENEMY_SPEED == 0 {
-        update_enemy_pos(enemies, player);
-    }
-    render_frame(buffer, enemies, *player);
+
+    update_teleporter_pos(teleport, key_state);
+    render_frame(buffer, teleport);
 }
 
-fn spawn_enemy(enemies: &mut [(u8, u8, u8); MAX_ENEMIES], rng: &mut impl Iterator<Item = u32>) {
-    let spawn_range_start = 64;
-    let spawn_range_end = 191;
+#[inline]
+fn place_teleporter(
+    last_position: (usize, usize),
+    rng: &mut impl Iterator<Item = u32>,
+    grid: &mut [bool; GRID_WIDTH * GRID_HEIGHT],
+) -> Option<(usize, usize)> {
+    let mut attempts = 0;
 
-    let spawn_range_width = (spawn_range_end - spawn_range_start) as u32;
+    while attempts < 100 {
+        let same_column = rng.next().unwrap() % 2 == 0;
+        let (new_x, new_y);
 
-    for _ in 0..ENEMIES_PER_WAVE {
-        if let Some(slot) = enemies.iter_mut().find(|e| e.2 == 0) {
-            let position_x =
-                (rng.next().unwrap() % spawn_range_width + spawn_range_start as u32) as u8;
-            let position = (position_x, 0);
-
-            *slot = new_enemy(position.0, position.1);
+        if same_column {
+            new_x = last_position.0;
+            let direction = rng.next().unwrap() % 7 + 5; // Generates a number between 5 and 11
+            new_y = (last_position.1 + direction as usize) % GRID_HEIGHT;
+        } else {
+            new_y = last_position.1;
+            let direction = rng.next().unwrap() % 7 + 5; // Generates a number between 5 and 11
+            new_x = (last_position.0 + direction as usize) % GRID_WIDTH;
         }
+
+        let is_valid_position = match same_column {
+            true => (new_y != last_position.1) && ((new_y + 1) % GRID_HEIGHT != last_position.1),
+            false => (new_x != last_position.0) && ((new_x + 1) % GRID_WIDTH != last_position.0),
+        };
+
+        let cell_index = new_y * GRID_WIDTH + new_x;
+
+        if !grid[cell_index] && is_valid_position {
+            grid[cell_index] = true;
+            return Some((new_x, new_y));
+        }
+
+        attempts += 1;
     }
+
+    None
 }
 
 #[inline]
-fn new_enemy(x: u8, y: u8) -> (u8, u8, u8) {
-    (x, y, 1)
-}
+fn spawn_teleporters_on_grid(
+    teleport: &mut [(u8, u8, u8); MAX_TELEPORT],
+    rng: &mut impl Iterator<Item = u32>,
+    grid: &mut [bool; GRID_WIDTH * GRID_HEIGHT],
+) {
+    let min_teleporters = 2;
+    let range = MAX_TELEPORT.saturating_sub(min_teleporters) + 1;
 
-enum Key {
-    Left,
-    Right,
-}
-
-#[inline]
-fn update_player_pos(player: &mut (u8, u8, u8), key_state: u8) {
-    let key = match key_state {
-        1 => Some(Key::Left),
-        2 => Some(Key::Right),
-        _ => None,
+    let num_teleporters = if range > 0 {
+        (rng.next().unwrap() % range as u32) as usize + min_teleporters
+    } else {
+        min_teleporters
     };
 
-    if let Some(key) = key {
-        match key {
-            Key::Left => player.0 = player.0.wrapping_sub(1),
-            Key::Right => player.0 = player.0.wrapping_add(1),
+    let mut last_position = (
+        rng.next().unwrap() as usize % GRID_WIDTH,
+        rng.next().unwrap() as usize % GRID_HEIGHT,
+    );
+
+    let initial_cell_index = last_position.1 * GRID_WIDTH + last_position.0;
+
+    grid[initial_cell_index] = true;
+
+    for i in 0..num_teleporters {
+        if i < teleport.len() {
+            let pos = if i == 0 {
+                last_position
+            } else if let Some(next_position) = place_teleporter(last_position, rng, grid) {
+                last_position = next_position;
+                next_position
+            } else {
+                continue;
+            };
+
+            teleport[i] = (
+                pos.0 as u8 * TELEPORT_SIZE as u8,
+                pos.1 as u8 * TELEPORT_SIZE as u8,
+                1,
+            );
+
+            if i == 0 {
+                teleport[i].2 = 2;
+            } else if i == 1 {
+                teleport[i].2 = 3;
+            }
         }
+    }
+
+    for i in num_teleporters..teleport.len() {
+        teleport[i].2 = 0;
     }
 }
 
 #[inline]
-fn update_enemy_pos(enemies: &mut [(u8, u8, u8); MAX_ENEMIES], player: &mut (u8, u8, u8)) {
-    for enemy in enemies.iter_mut() {
-        if enemy.1 == HEIGHT {
-            enemy.2 = 0;
-        }
+fn update_teleporter_pos(teleporters: &mut [(u8, u8, u8); MAX_TELEPORT], key_state: &mut bool) {
+    if *key_state {
+        if let Some((current, target)) = find_teleporter_targets(teleporters) {
+            let target_pos = (teleporters[target].0, teleporters[target].1);
+            let current_pos = &mut teleporters[current];
 
-        if enemy.2 == 0 {
-            continue;
-        }
+            let close_enough_x =
+                (current_pos.0 as i16 - target_pos.0 as i16).abs() <= TELEPORT_SPEED as i16;
+            let close_enough_y =
+                (current_pos.1 as i16 - target_pos.1 as i16).abs() <= TELEPORT_SPEED as i16;
 
-        enemy.1 += 1;
+            if close_enough_x && close_enough_y {
+                current_pos.0 = target_pos.0;
+                current_pos.1 = target_pos.1;
+                teleporters[current].2 = 0;
+                teleporters[target].2 = 2;
+                if target + 1 < teleporters.len() && teleporters[target + 1].2 != 0 {
+                    teleporters[target + 1].2 = 3;
+                }
+                *key_state = false;
+            } else {
+                if current_pos.0 < target_pos.0 {
+                    current_pos.0 += TELEPORT_SPEED;
+                } else if current_pos.0 > target_pos.0 {
+                    current_pos.0 -= TELEPORT_SPEED;
+                }
 
-        if (enemy.0 < player.0 + PLAYER_SIZE)
-            && (enemy.0 + ENEMY_SIZE > player.0)
-            && (enemy.1 < player.1 + PLAYER_SIZE)
-            && (enemy.1 + ENEMY_SIZE > player.1)
-        {
-            player.2 = 0;
+                if current_pos.1 < target_pos.1 {
+                    current_pos.1 += TELEPORT_SPEED;
+                } else if current_pos.1 > target_pos.1 {
+                    current_pos.1 -= TELEPORT_SPEED;
+                }
+            }
         }
     }
 }
 
-#[inline]
-fn render_frame(
-    buffer: &mut [u32; 255 * 255],
-    enemies: &[(u8, u8, u8); MAX_ENEMIES],
-    player: (u8, u8, u8),
-) {
-    if player.2 == 0 {
-        return;
+fn find_teleporter_targets(teleporters: &[(u8, u8, u8); MAX_TELEPORT]) -> Option<(usize, usize)> {
+    let mut current = None;
+    let mut next_target = None;
+
+    for (i, &tele) in teleporters.iter().enumerate() {
+        if tele.2 == 2 {
+            current = Some(i);
+            break;
+        }
     }
 
+    if let Some(curr) = current {
+        for tele in teleporters.iter().skip(curr + 1) {
+            if tele.2 != 0 {
+                next_target = Some(teleporters.iter().position(|&t| t == *tele).unwrap());
+                break;
+            }
+        }
+    }
+
+    if current.is_some() && next_target.is_none() {
+        next_target = teleporters.iter().position(|&t| t.2 != 0 && t.2 != 2);
+    }
+
+    match (current, next_target) {
+        (Some(c), Some(t)) => Some((c, t)),
+        _ => None,
+    }
+}
+
+#[inline]
+fn render_frame(buffer: &mut [u32; 255 * 255], teleporters: &mut [(u8, u8, u8); MAX_TELEPORT]) {
     buffer.fill(0xFF_00_00_00);
 
     let mut draw_rect = |x: u8, y: u8, width: u8, height: u8, color: u32| {
         for dy in 0..height {
             for dx in 0..width {
-                let index = usize::from(y + dy) * usize::from(WIDTH) + usize::from(x + dx);
+                let index = usize::from(y + dy) * WIDTH as usize + usize::from(x + dx);
                 if index < buffer.len() {
                     buffer[index] = color;
                 }
@@ -168,14 +258,41 @@ fn render_frame(
         }
     };
 
-    for enemy in enemies.iter() {
-        if enemy.2 == 0 {
-            continue;
+    for teleport in teleporters.iter() {
+        if teleport.2 == 1 {
+            draw_rect(
+                teleport.0,
+                teleport.1,
+                TELEPORT_SIZE,
+                TELEPORT_SIZE,
+                0xFF00FF00,
+            );
         }
-        draw_rect(enemy.0, enemy.1, ENEMY_SIZE, ENEMY_SIZE, 0xFFFFBF00);
     }
 
-    draw_rect(player.0, player.1, PLAYER_SIZE, PLAYER_SIZE, 0xFFFFFF);
+    for teleport in teleporters.iter() {
+        if teleport.2 == 3 {
+            draw_rect(
+                teleport.0,
+                teleport.1,
+                TELEPORT_SIZE,
+                TELEPORT_SIZE,
+                0xFFFFFF,
+            );
+        }
+    }
+
+    for teleport in teleporters.iter() {
+        if teleport.2 == 2 {
+            draw_rect(
+                teleport.0,
+                teleport.1,
+                TELEPORT_SIZE,
+                TELEPORT_SIZE,
+                0xFF_FF_00_FF,
+            );
+        }
+    }
 }
 
 #[panic_handler]
