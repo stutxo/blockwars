44c7bae9a899a6cb0315bf33b9e9b004594cc1f1
stutxo <70952638+stutxo@users.noreply.github.com>
2024-04-02 19:43:04 +0100

    remove complex spawn functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,14 @@
 #![no_std]
-use core::ptr;
+use core::{iter::repeat_with, ptr};
 
 const WIDTH: u8 = 255;
 const HEIGHT: u8 = 255;
 
 static mut TELEPORT: [(u8, u8, u8); MAX_TELEPORT] = [(0, 0, 0); MAX_TELEPORT];
-const MAX_TELEPORT: usize = 8;
+const MAX_TELEPORT: usize = 12;
 const TELEPORT_SIZE: u8 = 15;
 const TELEPORT_SPEED: u8 = 5;
 
-static mut GRID: [bool; GRID_WIDTH * GRID_HEIGHT] = [false; GRID_WIDTH * GRID_HEIGHT];
 const GRID_WIDTH: usize = (WIDTH as usize) / TELEPORT_SIZE as usize;
 const GRID_HEIGHT: usize = (HEIGHT as usize) / TELEPORT_SIZE as usize;
 
@@ -19,7 +18,7 @@ static mut PLAYER_MOVE: bool = false;
 #[inline]
 fn rng(seed: u32, frame: u32) -> impl Iterator<Item = u32> {
     let mut random = seed + frame;
-    core::iter::repeat_with(move || {
+    repeat_with(move || {
         random ^= random << 13;
         random ^= random >> 17;
         random ^= random << 5;
@@ -42,7 +41,6 @@ unsafe extern "C" fn game_loop(seed: u32, key_pressed: bool, frame: u32) {
         frame,
         seed,
         &mut *ptr::addr_of_mut!(TELEPORT),
-        &mut *ptr::addr_of_mut!(GRID),
         &mut *ptr::addr_of_mut!(PLAYER_MOVE),
     );
 }
@@ -54,108 +52,47 @@ fn frame_safe(
     frame: u32,
     seed: u32,
     teleport: &mut [(u8, u8, u8); MAX_TELEPORT],
-    grid: &mut [bool; GRID_WIDTH * GRID_HEIGHT],
     key_state: &mut bool,
 ) {
     let mut rng = rng(seed, frame);
 
     if frame == 1 {
-        spawn_teleporters_on_grid(teleport, &mut rng, grid);
+        spawn_tele(teleport, &mut rng);
     }
 
     if *key_state {
-        update_teleporter_pos(teleport, key_state);
+        update_tele_pos(teleport, key_state);
     }
     render_frame(buffer, teleport);
 }
 
 #[inline]
-fn place_teleporter(
-    last_position: (usize, usize),
-    rng: &mut impl Iterator<Item = u32>,
-    grid: &mut [bool; GRID_WIDTH * GRID_HEIGHT],
-) -> Option<(usize, usize)> {
-    let mut attempts = 0;
+fn spawn_tele(teleport: &mut [(u8, u8, u8); MAX_TELEPORT], rng: &mut impl Iterator<Item = u32>) {
+    let teleporter_size = TELEPORT_SIZE as usize;
+    let max_index_x = GRID_WIDTH - 1;
+    let max_index_y = GRID_HEIGHT - 1;
 
-    while attempts < 100 {
-        let same_column = rng.next().unwrap() % 2 == 0;
-        let (new_x, new_y);
-
-        if same_column {
-            new_x = last_position.0;
-            let direction = rng.next().unwrap() % 7 + 5; // Generates a number between 5 and 11
-            new_y = (last_position.1 + direction as usize) % GRID_HEIGHT;
-        } else {
-            new_y = last_position.1;
-            let direction = rng.next().unwrap() % 7 + 5; // Generates a number between 5 and 11
-            new_x = (last_position.0 + direction as usize) % GRID_WIDTH;
-        }
-
-        let is_valid_position = match same_column {
-            true => (new_y != last_position.1) && ((new_y + 1) % GRID_HEIGHT != last_position.1),
-            false => (new_x != last_position.0) && ((new_x + 1) % GRID_WIDTH != last_position.0),
-        };
-
-        let cell_index = new_y * GRID_WIDTH + new_x;
-
-        if !grid[cell_index] && is_valid_position {
-            grid[cell_index] = true;
-            return Some((new_x, new_y));
-        }
-
-        attempts += 1;
-    }
-
-    None
-}
-
-#[inline]
-fn spawn_teleporters_on_grid(
-    teleport: &mut [(u8, u8, u8); MAX_TELEPORT],
-    rng: &mut impl Iterator<Item = u32>,
-    grid: &mut [bool; GRID_WIDTH * GRID_HEIGHT],
-) {
-    let min_teleporters = 2;
-    let range = MAX_TELEPORT.saturating_sub(min_teleporters) + 1;
-
-    let num_teleporters = if range > 0 {
-        (rng.next().unwrap() % range as u32) as usize + min_teleporters
-    } else {
-        min_teleporters
-    };
-
-    let mut last_position = (
-        rng.next().unwrap() as usize % GRID_WIDTH,
-        rng.next().unwrap() as usize % GRID_HEIGHT,
-    );
-
-    let initial_cell_index = last_position.1 * GRID_WIDTH + last_position.0;
-
-    grid[initial_cell_index] = true;
+    let num_teleporters =
+        ((rng.next().unwrap() % (MAX_TELEPORT as u32 - 2)) as usize + 2).min(teleport.len());
 
     for i in 0..num_teleporters {
-        if i < teleport.len() {
-            let pos = if i == 0 {
-                last_position
-            } else if let Some(next_position) = place_teleporter(last_position, rng, grid) {
-                last_position = next_position;
-                next_position
-            } else {
-                continue;
-            };
+        let mut x = (rng.next().unwrap() as usize % max_index_x) * teleporter_size;
+        let mut y = (rng.next().unwrap() as usize % max_index_y) * teleporter_size;
 
-            teleport[i] = (
-                pos.0 as u8 * TELEPORT_SIZE as u8,
-                pos.1 as u8 * TELEPORT_SIZE as u8,
-                1,
-            );
+        x = x.min((WIDTH as usize) - teleporter_size);
+        y = y.min((HEIGHT as usize) - teleporter_size);
 
+        teleport[i] = (
+            x as u8,
+            y as u8,
             if i == 0 {
-                teleport[i].2 = 2;
+                1
             } else if i == 1 {
-                teleport[i].2 = 3;
-            }
-        }
+                2
+            } else {
+                3
+            },
+        );
     }
 
     for i in num_teleporters..teleport.len() {
@@ -164,7 +101,7 @@ fn spawn_teleporters_on_grid(
 }
 
 #[inline]
-fn update_teleporter_pos(teleporters: &mut [(u8, u8, u8); MAX_TELEPORT], key_state: &mut bool) {
+fn update_tele_pos(teleporters: &mut [(u8, u8, u8); MAX_TELEPORT], key_state: &mut bool) {
     if *key_state {
         if let Some((current, target)) = find_teleporter_targets(teleporters) {
             let target_pos = (teleporters[target].0, teleporters[target].1);
@@ -178,10 +115,10 @@ fn update_teleporter_pos(teleporters: &mut [(u8, u8, u8); MAX_TELEPORT], key_sta
             if close_enough_x && close_enough_y {
                 current_pos.0 = target_pos.0;
                 current_pos.1 = target_pos.1;
-                teleporters[current].2 = 0;
-                teleporters[target].2 = 2;
+                teleporters[current].2 = 4;
+                teleporters[target].2 = 1;
                 if target + 1 < teleporters.len() && teleporters[target + 1].2 != 0 {
-                    teleporters[target + 1].2 = 3;
+                    teleporters[target + 1].2 = 2;
                 }
                 *key_state = false;
             } else {
@@ -206,7 +143,7 @@ fn find_teleporter_targets(teleporters: &[(u8, u8, u8); MAX_TELEPORT]) -> Option
     let mut next_target = None;
 
     for (i, &tele) in teleporters.iter().enumerate() {
-        if tele.2 == 2 {
+        if tele.2 == 1 {
             current = Some(i);
             break;
         }
@@ -214,7 +151,7 @@ fn find_teleporter_targets(teleporters: &[(u8, u8, u8); MAX_TELEPORT]) -> Option
 
     if let Some(curr) = current {
         for tele in teleporters.iter().skip(curr + 1) {
-            if tele.2 != 0 {
+            if tele.2 != 0 && tele.2 != 4 {
                 next_target = Some(teleporters.iter().position(|&t| t == *tele).unwrap());
                 break;
             }
@@ -222,7 +159,9 @@ fn find_teleporter_targets(teleporters: &[(u8, u8, u8); MAX_TELEPORT]) -> Option
     }
 
     if current.is_some() && next_target.is_none() {
-        next_target = teleporters.iter().position(|&t| t.2 != 0 && t.2 != 2);
+        next_target = teleporters
+            .iter()
+            .position(|&t| t.2 != 0 && t.2 != 4 && t.2 != 1);
     }
 
     match (current, next_target) {
@@ -233,8 +172,6 @@ fn find_teleporter_targets(teleporters: &[(u8, u8, u8); MAX_TELEPORT]) -> Option
 
 #[inline]
 fn render_frame(buffer: &mut [u32; 255 * 255], teleporters: &mut [(u8, u8, u8); MAX_TELEPORT]) {
-    buffer.fill(0);
-
     let mut draw_rect = |x: u8, y: u8, width: u8, height: u8, state: u32| {
         for dy in 0..height {
             for dx in 0..width {
@@ -246,15 +183,18 @@ fn render_frame(buffer: &mut [u32; 255 * 255], teleporters: &mut [(u8, u8, u8); 
         }
     };
 
-    for teleport in teleporters.iter() {
-        let state = match teleport.2 {
-            1 => 1,
-            2 => 2,
-            3 => 3,
-            _ => 0,
-        };
-
-        draw_rect(teleport.0, teleport.1, TELEPORT_SIZE, TELEPORT_SIZE, state);
+    for state in (0..=4).rev() {
+        for teleport in teleporters.iter() {
+            if teleport.2 == state {
+                draw_rect(
+                    teleport.0,
+                    teleport.1,
+                    TELEPORT_SIZE,
+                    TELEPORT_SIZE,
+                    state.into(),
+                );
+            }
+        }
     }
 }
 
